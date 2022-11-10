8bd17419f80a4700ad568e29a5ef2fe86a947172
khazaddoom <31121986g@gmail.com>
2022-11-10 14:11:12 +0530

    user schema added
    todo: OTP API

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -1,20 +1,40 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { PrismaClient } from '@prisma/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { SUCCESS } from './types'
 
-
-type Data = {
-  users: any[]
-}
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<any>
 ) {
-  const prisma = new PrismaClient()
-  // const allUsers = await prisma.user.findMany()
-  // console.log(allUsers)
+  const { method, body: {country_code, mobile_number} } = req
+  let result = {}
+  const prisma = new PrismaClient();
+  switch (method) {
+    case 'GET':
+      result = {}
+      break;
+    case 'POST':
+      await prisma.user.findMany({
+        
+      })
+      const user = await prisma.user.create({
+        data: {
+          namedetails: [{
+            country_code,
+            mobile_number,
+            otp: Math.round(Math.random()*1000),
+            created_on: Date.now()
+          }]
+        }
+      })
+      result = { data: {...user}}
+      break;
+    default:
+      break;
+  }
 
-  res.status(200).json({ users: await prisma.users.findMany() })
+  res.status(200).json({ ...SUCCESS, ...result })
 }
 
